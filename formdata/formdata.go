798826b0410875d0aa7e1b6c79c6bc8d0ddb9111
Laily <i@laily.net>
2021-01-29 19:48:49 +0800

    feat: for setup a form data request body

---
+++ formdata/formdata.go
@@ -0,0 +1,49 @@
+package formdata
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"mime/multipart"
+)
+
+// NewFormBody represent create a http form data request
+// @param params represent fields
+// @param files represent update files
+// https://zhuanlan.zhihu.com/p/96491484
+func NewFormBody(params map[string]string, files []*FileInfo) (contentType string, body io.Reader, err error) {
+	buf := &bytes.Buffer{}
+	writer := multipart.NewWriter(buf)
+
+	defer writer.Close()
+
+	for _, v := range files {
+		fileWriter, err := writer.CreateFormFile(v.Fieldname, v.Filename)
+		if err != nil {
+			err = fmt.Errorf("write file field, key: %s, val: %s, %w", v.Fieldname, v.Filename, err)
+			return
+		}
+		_, err = io.Copy(fileWriter, v.Data)
+		if err != nil {
+			err = fmt.Errorf("write file, key: %s, val: %s, %w", v.Fieldname, v.Filename, err)
+			return
+		}
+	}
+
+	for k, v := range params {
+		err = writer.WriteField(k, v)
+		if err != nil {
+			err = fmt.Errorf("write field, key: %s, val: %s, %w", k, v, err)
+			return
+		}
+	}
+
+	return writer.FormDataContentType(), buf, nil
+}
+
+// FileInfo for update file info
+type FileInfo struct {
+	Fieldname string
+	Filename  string
+	Data      io.Reader
+}
