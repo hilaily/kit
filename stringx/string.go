139f23773ea7f6bf8620a78ffb4df23c308bfef9
Laily <i@laily.net>
2021-10-25 16:52:52 +0800

    feat: add cast function for string

--- stringx/string.go
+++ stringx/string.go
@@ -1,19 +1,101 @@
 package stringx
 
 import (
+	"bytes"
 	"crypto/x509"
 	"encoding/pem"
 	"errors"
 	"fmt"
+	"log"
 	"path"
 	"regexp"
+	"strconv"
 	"strings"
+	"unicode"
 	"unsafe"
 )
 
 var (
 	compresReg *regexp.Regexp
 )
+
+// Camel2Case ...
+func Camel2Case(name string) string {
+	buffer := NewBuffer()
+	for i, r := range name {
+		if unicode.IsUpper(r) {
+			if i != 0 {
+				buffer.Append('_')
+			}
+			buffer.Append(unicode.ToLower(r))
+		} else {
+			buffer.Append(r)
+		}
+	}
+	return buffer.String()
+}
+
+// Case2Camel ...
+func Case2Camel(name string) string {
+	name = strings.ReplaceAll(name, "_", " ")
+	name = strings.Title(name)
+	return strings.ReplaceAll(name, " ", "")
+}
+
+// Ucfirst ...
+func Ucfirst(str string) string {
+	for i, v := range str {
+		return string(unicode.ToUpper(v)) + str[i+1:]
+	}
+	return ""
+}
+
+// Lcfirst
+func Lcfirst(str string) string {
+	for i, v := range str {
+		return string(unicode.ToLower(v)) + str[i+1:]
+	}
+	return ""
+}
+
+// 内嵌bytes.Buffer，支持连写
+type Buffer struct {
+	*bytes.Buffer
+}
+
+func NewBuffer() *Buffer {
+	return &Buffer{Buffer: new(bytes.Buffer)}
+}
+
+func (b *Buffer) Append(i interface{}) *Buffer {
+	switch val := i.(type) {
+	case int:
+		b.append(strconv.Itoa(val))
+	case int64:
+		b.append(strconv.FormatInt(val, 10))
+	case uint:
+		b.append(strconv.FormatUint(uint64(val), 10))
+	case uint64:
+		b.append(strconv.FormatUint(val, 10))
+	case string:
+		b.append(val)
+	case []byte:
+		b.Write(val)
+	case rune:
+		b.WriteRune(val)
+	}
+	return b
+}
+
+func (b *Buffer) append(s string) *Buffer {
+	defer func() {
+		if err := recover(); err != nil {
+			log.Println("out of memory")
+		}
+	}()
+	b.WriteString(s)
+	return b
+}
 
 // StringToBytes ...
 func StringToBytes(s string) []byte {
