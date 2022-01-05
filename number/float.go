3c4f7e65ddaf8b89669f51aa88894bb8b372eeba
Laily <i@laily.net>
2022-01-05 11:27:38 +0800

    fix: format float

--- number/float.go
+++ number/float.go
@@ -4,9 +4,13 @@ import (
 	"strconv"
 )
 
-// FloatPrecision float 小数点保留指定位数
-func FloatPrecision(value float64, prec int) float64 {
-	pp := strconv.FormatFloat(value, 'f', prec, 64)
-	value, _ = strconv.ParseFloat(pp, 64)
+// FormatFloat 小数点保留指定位数
+func FormatFloat(value float64, decimal int) float64 {
+	value, _ = EFormatFloat(value, decimal)
 	return value
+}
+
+func EFormatFloat(value float64, decimal int) (float64, error) {
+	pp := strconv.FormatFloat(value, 'f', decimal, 64)
+	return strconv.ParseFloat(pp, 64)
 }
