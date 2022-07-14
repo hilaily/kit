634abf3ff93af6b9faa818324ef73fb8884723c6
longbin <longbin.go@bytedance.com>
2022-07-14 21:30:02 +0800

    fix: add unwrap error

--- dev/err.go
+++ dev/err.go
@@ -1,10 +1,26 @@
 package dev
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/sirupsen/logrus"
 )
+
+func Unwrap[T any](err error) (T, bool) {
+	var current = err
+	var last = err
+	for {
+		current = errors.Unwrap(current)
+		if current == nil {
+			err = last
+			break
+		}
+		last = current
+	}
+	v, ok := err.(T)
+	return v, ok
+}
 
 type devTool struct {
 	log logrus.Entry
