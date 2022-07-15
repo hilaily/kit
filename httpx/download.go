cf668d37c67628f5aa0e78bad314ac7036ca183c
longbin <longbin.go@bytedance.com>
2022-07-15 16:22:05 +0800

    feat: add download file

---
+++ httpx/download.go
@@ -0,0 +1,14 @@
+package httpx
+
+import (
+	"io"
+	"net/http"
+)
+
+func DownloadHander(resp http.ResponseWriter, reader io.Reader, filename string) error {
+	resp.Header().Add("Content-Description", "File Transfer")
+	resp.Header().Add("Content-Disposition", "attachment; filename="+filename)
+	resp.Header().Add("Content-Type", "application/octet-stream")
+	_, err := io.Copy(resp, reader)
+	return err
+}
---
+++ httpx/download_test.go
@@ -0,0 +1,37 @@
+package httpx
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDownload(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	w := httptest.NewRecorder()
+
+	server := http.NewServeMux()
+	server.HandleFunc("/", handler)
+
+	server.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+	data, _ := ioutil.ReadAll(w.Body)
+	fmt.Println(string(data))
+
+	header := w.Header()
+	for k, v := range header {
+		t.Log(k, v)
+	}
+}
+
+func handler(resp http.ResponseWriter, req *http.Request) {
+	data := &bytes.Buffer{}
+	data.WriteString("a test file")
+	DownloadHander(resp, data, "test.txt")
+}
