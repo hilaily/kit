78f6658e1eea3054db7605995a13fcc4809e29e1
Laily <i@laily.net>
2023-08-28 15:35:08 +0800

    feat(mapx): add reverse

---
+++ mapx/common.go
@@ -0,0 +1,10 @@
+package mapx
+
+// Reverse map key val
+func Reverse[K comparable, V comparable](in map[K]V) map[V]K {
+	out := make(map[V]K, len(in))
+	for k, v := range in {
+		out[v] = k
+	}
+	return out
+}
